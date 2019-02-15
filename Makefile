0a630cdf94b6bd4d9d222e030582096dff7d2081
Frederick Boehm <frederick.boehm@gmail.com>
2019-02-15 14:57:11 -0600

    added manuscript tex & needed files

---
+++ Makefile
@@ -0,0 +1,46 @@
+RMD_FILE = Rmd/2018-11-17_recla-figures-revised.Rmd
+
+# Tools
+LATEXMK = latexmk
+RM = rm -f
+
+# Project-specific settings
+DOCNAME = overleaf-repo/main
+
+# Targets
+all: doc
+doc: pdf
+pdf: overleaf-repo/main.pdf
+fig: R/power-curves.eps Rmd/scatter.eps Rmd/chr8-lods.eps Rmd/coefs.eps Rmd/profile.eps Rmd/genomewide_lod_trait10.eps Rmd/genomewide_lod_trait22.eps
+
+
+# Rules
+%.pdf: %.tex fig
+	cd overleaf-repo; $(LATEXMK) -pdfps -bibtex main.tex
+mostlyclean:
+	cd overleaf-repo; $(LATEXMK) -silent -c
+	$(RM) overleaf-repo/*.bbl
+
+clean: mostlyclean
+	cd overleaf-repo; $(LATEXMK) -silent -C
+	$(RM) overleaf-repo/*.run.xml overleaf-repo/*.synctex.gz
+	$(RM) overleaf-repo/*.d
+
+.PHONY: all clean doc mostlyclean pdf
+
+# Include auto-generated dependencies
+#-include *.d
+
+
+R/power-curves.eps: R/plot-power-curves.R
+	R CMD BATCH R/plot-power-curves.R
+
+Rmd/scatter.eps: $(RMD_FILE)
+	Rscript -e "rmarkdown::render('$<')"
+
+# use latexdiff to get PDF comparing current to an older version
+diff:
+	latexdiff latexdiff/main_old.tex overleaf-repo/main.tex > latexdiff/diff.tex
+	mv latexdiff/diff.tex overleaf-repo
+	cd overleaf-repo;$(LATEXMK) -pdfps -bibtex diff.tex
+	mv overleaf-repo/diff* latexdiff/
---
+++ R/plot-power-curves.R
@@ -0,0 +1,35 @@
+# from page 197-198 on google drive research notebook
+
+trace <- rep(1:4, each = 5)
+distance <- rep(c(0, 0.5, 1, 2, 3), times = 4)
+power <- c(0.0425, 0.61, 0.8575, 0.935, 0.985,
+           0.0575, 0.96, 1, 1, 1,
+           0.05, 0.4175, 0.6375, 0.7725, 0.8775,
+           0.0425, 0.8125, 0.98, 1, 1
+           )
+partition <- rep(c("ABCD:EFGH", "F:ABCDEGH"), each = 10)
+effect <- rep(c(1, 2, 1, 2), each = 5)
+interaction <- paste(effect, partition, sep = ", ") %>% as.factor()
+library(dplyr)
+
+tibble(trace, distance, power, effect, partition, interaction) -> mytib
+
+
+
+library(ggplot2)
+mytib %>%
+  #group_by(trace) %>%
+  ggplot() + geom_line(aes(x = distance, 
+                           y = power, 
+                           colour = interaction, 
+                           linetype = interaction)) +
+  broman::theme_karl() +
+  theme(legend.position = c(0.85, 0.23
+                            )) + 
+  xlab("Distance (cM)") + ylab("Power") +
+  scale_linetype_manual("Effect size, Allele partitioning", values=c(1, 2, 1, 2)) +
+  scale_colour_manual("Effect size, Allele partitioning", values=c(1, 1, 2, 2)) + 
+  theme(
+        legend.title=element_text(size=8), 
+        legend.text=element_text(size=7))
+ ggsave("R/power-curves.eps", height=4, width=7.5)
